24742e3c3ade88666c6fd3b8a68a234509d237bf
Galina <galinka-ver@mail.ru>
2023-01-29 09:38:30 +0500

    Среднее арифметическое столбца

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int row = new Random().Next(1,5);;
+int col = new Random().Next(1,5);;
+int[,] array = new int[row,col];
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+    array[i,j] = new Random().Next(1,100);
+    Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+for (int p = 0; p < col; p++)
+{
+    double sum = 0;
+    for (int k = 0; k < row; k++)
+    {
+        sum = sum+array[k,p];
+    }
+    double d = sum/row;
+    Console.WriteLine($"Среднее арифметическое {p+1} столбца = {d}");
+}
